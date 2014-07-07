9ff989038175dd2163f17c645e78a60132b64f3e
guiem <g@guiem.info>
2014-07-07 19:09:41 +0200

    added angular-translate

--- website/public/js/core.js
+++ website/public/js/core.js
@@ -1,1 +1,13 @@
-angular.module('expdemAnalytics', ['userService','tweetService','wordService','expdemController','hashtagService']);
+var app = angular.module('expdemAnalytics', ['userService','tweetService','wordService',
+	'expdemController','hashtagService','pascalprecht.translate'])
+
+app.config(['$translateProvider', function($translateProvider) {
+	$translateProvider
+    .translations('es', {
+      'DROPDOWN_PROYECTOS': 'Proyectos',
+    })
+    .translations('en', {
+      'DROPDOWN_PROYECTOS': 'Projects',
+    })
+    .preferredLanguage('es');
+}])
