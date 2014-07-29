6a660271ece7cc95b5c83c3fe4ccd75d243fafd9
guiem <g@guiem.info>
2014-07-29 18:37:11 +0200

    Changed method to compute n-hashtags, now it reads the tweet['entities']['hashtags'] instead of processing tweet's text

--- twitterTools/create_nhashtags.py
+++ twitterTools/create_nhashtags.py
@@ -4,7 +4,6 @@ from utils import *
 import pymongo
 import datetime
 import time
-import nltk
 
 # connect to mongo
 connection = pymongo.Connection("mongodb://{0}".format(DB_URL), safe=True)
@@ -16,17 +15,16 @@ hashtags = eval("db.hashtags{0}".format(HASHTAGS_SUFIX))
 tweets_processed = 0
 res = {}
 for tweet in tweets.find():
-    hashtag_list = [word for word in tweet['text'].split() if word.startswith('#')]
-    for h in hashtag_list:
-        if h.lower() not in res.keys():
-            res[h.lower()] = 1
-        else:
-            res[h.lower()] += 1
+    if 'entities' in tweet.keys() and tweet['entities']:
+        for entity in tweet['entities']['hashtags']:
+            entity = entity['text'].encode('utf-8')
+            if entity not in res:
+                res[entity.lower()] = 0
+            res[entity.lower()] += 1
     tweets_processed += 1
     print """Total: {0}""".format(tweets_processed)
 hashtags_db = 0
-for k in res.keys():
+for i,k in enumerate(res):
     hashtags.insert({"hashtag":k,"count":res[k]})
-    hashtags_db += 1
-    print """Total hashtags db: {0}""".format(hashtags_db)
+    print """Total hashtags db: {0}""".format(i+1)
 
