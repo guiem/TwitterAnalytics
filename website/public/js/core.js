36c28ff8ebbfd3a17044c8a78f6f3bc8e30c26d5
guiem <g@guiem.info>
2014-09-03 23:32:55 +0200

    a few content changes in home view

--- website/public/js/core.js
+++ website/public/js/core.js
@@ -28,6 +28,8 @@ app.config(['$translateProvider', function($translateProvider) {
       'COLABORA': 'Colabora',
       'CONTACTO': 'Contacto',
       'DESCARGA_APP':'Descarga App',
+      'DESARROLLO': 'La aplicación de escritorio está de momento en <strong>fase de desarrollo</strong>. ¡Muy pronto podrás descargar la primera versión beta!',
+      'APP_ESCRITORIO':"<strong>Recuperar/analizar</strong> los tweets puede ser un proceso de varios días. Debido a que esto conlleva un uso considerable de recursos de computación se ha optado por desarrollar una App que pueda ser <strong>usada en el ordenador personal</strong> de cada usuario.",
     })
     .translations('en', {
       'PROYECTOS': 'Projects',
@@ -36,6 +38,8 @@ app.config(['$translateProvider', function($translateProvider) {
       'COLABORA': 'Take Part',
       'CONTACTO': 'Contact',
       'DESCARGA_APP':'Download App',
+      'DESARROLLO':"Desktop App is still <strong>under developement</strong>. Hopefully you'll be able to download it very soon!",
+      'APP_ESCRITORIO':"<strong>Retrieving/analyzing</strong> tweets might involve several days of data processing. In order to avoid stressing too much one single server, a Desktop App has been developed so users can run their projects from any <strong>personal computer</strong>."
     })
     .preferredLanguage('es');
 }])
