b58c8cd9286ab9096087ef638fcc829a259e6dde
guiem <g@guiem.info>
2014-10-19 17:59:41 +0200

    fixed limit to pause

--- twitterTools/utils.py
+++ twitterTools/utils.py
@@ -1,7 +1,7 @@
 import datetime
 
 def needs_sleep(limit_remaining,limit_reset):
-    if limit_remaining <= 0:
+    if limit_remaining <= 1:
         reset_time = datetime.datetime.fromtimestamp(float(limit_reset))
         now = datetime.datetime.now()
         seconds_to_sleep = (reset_time - now).seconds
