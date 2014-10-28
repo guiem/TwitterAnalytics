d18c7f0747e6d8f54687ca378d4a100b132142b3
guiem <g@guiem.info>
2014-10-28 19:34:27 +0100

    solving layouts, done until tweets in time graph

--- website/public/js/charts/tweetsperday.js
+++ website/public/js/charts/tweetsperday.js
@@ -7,7 +7,7 @@ function drawTweetsPerDayChart(dataAux) {
         //title: 'Tweets por d’a',
         legend: { position: 'bottom' },
         hAxis: { textPosition: 'none' },
-        width:1000,
+        //width:1000,
         height:500,
     };
     var chart = new google.visualization.LineChart(document.getElementById('tweets-per-day-chart'));
