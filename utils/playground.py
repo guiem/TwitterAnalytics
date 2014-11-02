3908038fdff6dd086992aaff80e4b2926db66920
guiem <g@guiem.info>
2014-11-02 12:51:04 +0100

    first export to gephi hashtag version

--- utils/playground.py
+++ utils/playground.py
@@ -55,9 +55,37 @@ def _get_hashtags_users(num_hashtags):
         num_hash += 1
     return res
 
-def hashtagsgraph():
+def hashtagsgraph(num_hashtags, filepath):
     hash_dict = _get_hashtags_users(num_hashtags)
-    pass
+    main_start = """
+    <gexf xmlns="http://www.gexf.net/1.2draft" xmlns:viz="http://www.gexf.net/1.2draft/viz" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.gexf.net/1.2draft http://www.gexf.net/1.2draft/gexf.xsd" version="1.2">
+        <creator>Guiem</creator>
+        <description>from TwitterAnalytics</description>
+    """
+    main_end = """</gexf>"""
+    graph_start = """<graph mode="static" defaultedgetype="directed">"""
+    graph_end = """</graph>"""
+    nodes_start = """<nodes>"""
+    nodes_end = """</nodes>"""
+    edges_start = """<edges>"""
+    edges_end = """</edges>"""
+    nodes = []
+    edges = []
+    for hash in hash_dict:
+        node = """<node id="{0}" label="{0}"/>""".format(hash)
+        if node not in nodes:
+            nodes.append(node)
+        for user in hash_dict[hash]['users']:
+            sub_node = """<node id="{0}" label="{0}"/>""".format(user)
+            if sub_node not in nodes:
+                nodes.append(sub_node)
+            edge = """<edge id="{0}-{1}" source="{0}" target="{1}"/>""".format(user,hash)
+            if edge not in edges:
+                edges.append(edge)   
+    res = main_start+graph_start+nodes_start+('').join(nodes)+nodes_end+edges_start+('').join(edges)+edges_end+graph_end+main_end
+    f = open(filepath,'w')
+    f.write(res)        
+    f.close()
 
 def listhashtags(num_hashtags, filepath):
     hash_dict = _get_hashtags_users(num_hashtags)
